04e5b646282d42f4a5a528dcd5b3816b58e757a8
Aatrox77 <112231881+Aatrox77@users.noreply.github.com>
2022-09-11 12:34:39 +0900

    Add files via upload

---
+++ 4_2_text.py
@@ -0,0 +1,17 @@
+from dataclasses import replace
+from traceback import print_tb
+
+
+print('Hello world')
+print("Hello world")
+print('''
+Hello
+world
+'''
+)
+
+print("'1'+'1'", '1'+'1')
+print('Hello world'*1000)
+print("len('Hello world'*1000)", len('Hello world'*1000))
+print("'Hello world'.replace('world', 'universe'", 'Hello world'.
+replace('world','universe'))
